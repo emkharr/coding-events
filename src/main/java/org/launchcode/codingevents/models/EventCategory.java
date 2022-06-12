5cb2422093f9f7576a2d47a5697cd5a69415892c
emkharr <ekharrison.art@gmail.com>
2022-06-12 14:07:00 -0600

    added event details

---
+++ src/main/java/org/launchcode/codingevents/models/EventCategory.java
@@ -0,0 +1,41 @@
+package org.launchcode.codingevents.models;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class EventCategory extends AbstractEntity{
+
+    @NotBlank(message = "Name is required")
+    @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters")
+    private String name;
+
+    @OneToMany(mappedBy = "eventCategory")
+    private final List<Event> events = new ArrayList<>();
+
+    public EventCategory(@Size(min=3, message="Name must be at least 3 characters in length") String name) {
+        this.name = name;
+    }
+
+    public EventCategory(){};
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Event> getEvents() {
+        return events;
+    }
+
+    @Override
+    public String toString() {return name;}
+
+}
